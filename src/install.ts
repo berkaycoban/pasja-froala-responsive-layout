ca20f7539f0213acb2f3afbafe493d39b13d3096
Berkay COBAN <bcobann35@gmail.com>
2022-06-24 18:09:07 +0300

    fix: Add align component in install.ts

--- src/install.ts
+++ src/install.ts
@@ -1,9 +1,11 @@
 import type { App } from 'vue'
 
 import PasjaFroalaGrid from './components/PasjaFroalaGrid.vue'
+import PasjaFroalaAlign from './components/PasjaFroalaAlign.vue'
 
 export default {
   install: (app: App) => {
     app.component('PasjaFroalaGrid', PasjaFroalaGrid)
+    app.component('PasjaFroalaAlign', PasjaFroalaAlign)
   }
 }
